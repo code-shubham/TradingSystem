409828e9ebbab692be3dc38a3166d692788dce4d
code-shubham <47816489+code-shubham@users.noreply.github.com>
2020-07-03 14:05:25 +0530

    Adding Trading System server

---
+++ TradingSystemServ/TradingSystemServ/Models/Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingSystemServ.Models
+{
+    public class Model
+    {
+        public string Buyer { get; set; }
+        public string Seller { get; set; }
+
+        public int Qty { get; set; }
+        public int Price { get; set; }
+        public string Symbol { get; set; }
+        public int MyBidQty { get; set; }
+        public int MktBidQty { get; set; }
+      
+        public int MyAskQty { get; set; }
+        public int MktAskQty { get; set; }
+    }
+}
